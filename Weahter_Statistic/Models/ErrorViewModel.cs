b202f0c442ce82a70232e56c7d5b8db03796e9c5
Karol Ścigała <karols96@interia.pl>
2019-03-17 15:15:11 +0100

    Dodaj pliki projektów.

---
+++ Weahter_Statistic/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Weahter_Statistic.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
