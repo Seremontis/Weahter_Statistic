5488c8c92e145a5ec7c6a7b38d61723dfc4d6a52
Karol Ścigała <seremontis1996@gmail.com>
2019-03-19 13:31:09 +0100

    upgrade menu slide

--- Weahter_Statistic/wwwroot/js/mainpageJS.js
+++ Weahter_Statistic/wwwroot/js/mainpageJS.js
@@ -1,5 +1,34 @@
-﻿var el = document.querySelectorAll(".textmenu");
-//el.style.color = "red";
+﻿var menuclick = document.querySelector(".menu");
+var menu = document.querySelector("nav");
+var bgmenu = document.querySelector(".bgmenu");
+
+menuclick.addEventListener("click", showMenu);
 
 
-document.getElementById("textmenu").style.color = "blue";
+window.onclick = function (event) {
+    if (event.target== menu) {
+        menu.style.right = "-400px";
+        menu.style.transition = "2s ease";
+        menuclick.style.right = "0";
+        menuclick.style.transition = "2s ease";
+        bgmenu.display = "block";
+    }
+}
+
+function showMenu() { 
+    if (menuclick.style.right == "0px" || menuclick.style.right == "") {
+        menu.style.right = "0px";
+        menu.style.transition = "2s ease";
+        menuclick.style.right = "400px";
+        menuclick.style.transition = "2s ease";
+        bgmenu.display = "block";
+    }
+    else {
+        menu.style.right = "-400px";
+        menu.style.transition = "2s ease";
+        menuclick.style.right = "0";
+        menuclick.style.transition = "2s ease";
+        bgmenu.display = "none";
+    }
+}
+
